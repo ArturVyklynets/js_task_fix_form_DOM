82a9d9e82d607a3572ce42aa43e6865f2d107a67
Artur <artur2005456@gmail.com>
2025-01-13 17:28:33 +0200

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const collectionOfInputs = [...document.querySelectorAll('input')];
+
+collectionOfInputs.map((element) => {
+  const nameForPlaceHolder =
+    element.name.charAt(0).toUpperCase() +
+    element.name.slice(1, element.name.length);
+
+  element.insertAdjacentHTML(
+    'beforebegin',
+    `<label class='field-label' for=${element.id}>${element.name.toUpperCase()}</label>`,
+  );
+  element.setAttribute('placeholder', nameForPlaceHolder);
+});
